0212f05e9dba583498720448d70c9268b51d3d4e
Benjamin Nicholas Overgaard <tarious@live.dk>
2015-05-01 23:42:07 +0200

    Gained control of the monster. Sorted scripts. Got new pictures working.

---
+++ Assets/Scripts/AlienManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienManager : MonoBehaviour {
+    /*
+    private int idleHash = Animator.StringToHash("Idle");
+    private int thinkingHash = Animator.StringToHash("Thinking");
+    private int sleepingHash = Animator.StringToHash("Sleeping");
+    private int greatingHash = Animator.StringToHash("Greating");
+    private int instructingHash = Animator.StringToHash("Instructing");
+    */
+    
+    public string[] bodyAnimations;
+    public string[] mouthAnimations;
+    public Sprite[] speechSprites;
+
+    private SpriteRenderer _speechSprite;
+    private Animator _bodyAnimator;
+    private Animator _mouthAnimator;
+
+    private string prevState;
+
+    void OnEnable()
+    {
+        if (MainManager.instance != null)
+            MainManager.instance.OnDialogueEvent += OnDialogClick;
+    }
+
+    void OnDisable()
+    {
+        if (MainManager.instance != null)
+            MainManager.instance.OnDialogueEvent -= OnDialogClick;
+    }
+    
+    // Use this for initialization
+	void Start ()
+	{
+	    if (transform.FindChild("SpeechBubble").GetComponent<SpriteRenderer>() != null)
+            _speechSprite = transform.FindChild("SpeechBubble").GetComponent<SpriteRenderer>();
+	    else
+	        print("YOU NEED TO ADD A SPEECHBUBBLE");
+
+        _bodyAnimator = GameObject.FindGameObjectWithTag("MonsterContainer").GetComponent<Animator>();
+        _mouthAnimator = GameObject.FindGameObjectWithTag("Mouth").GetComponent<Animator>();
+
+        _bodyAnimator.SetBool(Animator.StringToHash(bodyAnimations[0]), true);
+	    prevState = bodyAnimations[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDialogClick(int clickCount)
+    {
+        _speechSprite.sprite = speechSprites[clickCount];
+
+        _bodyAnimator.SetBool(Animator.StringToHash(prevState), false);
+        _bodyAnimator.SetBool(Animator.StringToHash(bodyAnimations[clickCount]), true);
+        prevState = bodyAnimations[clickCount];
+        //_mouthAnimator.SetBool(Animator.StringToHash(mouthAnimations[clickCount]), true);
+    }
+}
+
+
---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainManager : MonoBehaviour {
+
+    public static MainManager instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = GameObject.FindObjectOfType<MainManager>();
+            return _instance;
+        }
+    }
+
+    private static MainManager _instance;
+
+    private State currentState = State.None;
+    private State prevState;
+
+    public delegate void DialogueEvent(int clickCount);
+    public event DialogueEvent OnDialogueEvent;
+
+    [HideInInspector]
+    public float Points;
+
+    private int _dialogueClicks = 2;
+    private int _dialogueClickCount = 0;
+
+    public Canvas currentCanvas;
+
+    public enum State
+    {
+        None = -1,
+        Logging = 0,
+        Start = 1,
+        AR = 2,
+        BearDialogue = 3,
+        Riddles = 4,
+        Map = 5,
+        KidDialogue = 6,
+        End = 7
+    }
+
+    public State CurrentState
+    {
+        get { return currentState; }
+        set
+        {
+            prevState = currentState;
+            currentState = value;
+
+            switch (currentState)
+            {
+                case State.Start:
+                    _dialogueClicks = 4;
+                    break;
+                case State.BearDialogue:
+                    _dialogueClicks = 5;
+                    break;
+                case State.KidDialogue:
+                    _dialogueClicks = 7;
+                    break;
+            }
+        }
+    }
+
+    void OnLevelWasLoaded()
+    {
+        _dialogueClickCount = 0;
+
+        if (GameObject.Find("Canvas") != null)
+            currentCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+    }
+        
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDialogueButtonClick()
+    {
+        if (_dialogueClickCount < _dialogueClicks)
+        {
+            _dialogueClickCount++;
+            OnDialogueEvent(_dialogueClickCount);
+        }
+        else
+        {
+            LoadNextScene();
+        }
+    }
+
+    void LoadNextScene()
+    {
+        print("Load Next Scene");
+    }
+}
